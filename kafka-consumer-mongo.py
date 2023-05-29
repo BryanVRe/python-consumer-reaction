bd55318ac8d0a8571780ea1b57668805699617e1
BryanVRe <fsocietybvr@gmail.com>
2023-05-28 19:44:00 -0600

    create new repository and new summary

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -9,6 +9,7 @@ from pymongo.server_api import ServerApi
 import json
 
 uri = "mongodb+srv://fsocietybvr:BryanVRe@cluster0.dor0cxy.mongodb.net/?retryWrites=true&w=majority";
+
 #Local = "mongodb://127.0.0.1:27017"
 #URL del profe = "mongodb+srv://adsoft:adsoft-sito@cluster0.kzghgph.mongodb.net/?retryWrites=true&w=majority"
 
@@ -33,42 +34,53 @@ try:
     print("MongoDB Connected successfully!")
 except:
     print("Could not connect to MongoDB")
-consumer = KafkaConsumer('test',bootstrap_servers=['my-kafka.bryanvre.svc.cluster.local:9092'])#'my-kafka-0.my-kafka-headless.kafka-adsoftsito.svc.cluster.local:9092'])
+
+
+consumer = KafkaConsumer('reactions',bootstrap_servers=['my-kafka.bryanvre.svc.cluster.local:9092'])
+#'my-kafka-0.my-kafka-headless.kafka-adsoftsito.svc.cluster.local:9092'])
 # Parse received data from Kafka
 for msg in consumer:
     record = json.loads(msg.value)
     print(record)
-    name = record['name']
+    userId = record["userId"]
+    objectId = record["objectId"]
+    reactionId = record["reactionId"]
 
     # Create dictionary and ingest data into MongoDB
     try:
-       meme_rec = {'name':name }
-       print (meme_rec)
-       meme_id = db.memes_info.insert_one(meme_rec)
-       print("Data inserted with record ids", meme_id)
+       reaction_rec = {
+            'userId': userId,
+            'objectId': objectId,
+            'reactionId': reactionId
+        }
+       print (reaction_rec)
+       reaction_id = db.memes_reactions.insert_one(reaction_rec)
+       print("Data inserted with record ids", reaction_id)
     except:
        print("Could not insert into MongoDB")
 
+  # Create bdnosql_sumary and insert groups into mongodb
     try:
-        agg_result = db.memes_info.aggregate(
-            [{
-                "$group" : 
-                { "_id" : "$name",
-                  "n" : {"$sum":1}}
-            }]
-        )
-        db.memes_summary.delete_many({})
+        agg_result = db.memes_reactions.aggregate([
+        {
+            "$group": {
+                "_id": {
+                    "objectId": "$objectId",
+                    "reactionId": "$reactionId"
+                },
+                "n": {"$sum": 1}
+            }
+        }
+    ])
+        db.memes_sumaryReactions.delete_many({})
         for i in agg_result:
             print(i)
-            summary_id = db.memes_summary.insert_one(i)
-            print("Summary inserted with record ids", summary_id)
-       #meme_rec = {'name':name }
-       #print (meme_rec)
-       #meme_id = db.memes_info.insert_one(meme_rec)
-       #print("Data inserted with record ids", meme_id)
+            sumary_id = db.memes_sumaryReactions.insert_one(i)
+            print("Sumary Reactions inserted with record ids: ", sumary_id)
+            
     except Exception as e:
-        print(f'group by caught {type(e)}: ')
+        print(f'group vy cought {type(e)}: ')
         print(e)
-       #print("Could not insert into MongoDB")
+
 
 
